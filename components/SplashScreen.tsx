6bd03c226af59d53ba9d91efedfae831475e156a
padhashubham2-svg <padhashubham2-svg@users.noreply.github.com>
2026-08-24 13:14:02 +0100

    Splash: white text color, 120% larger logo

--- components/SplashScreen.tsx
+++ components/SplashScreen.tsx
@@ -33,8 +33,8 @@ export default function SplashScreen() {
             <Image
               src="/vaskoi_logo.png"
               alt="Vaskoi"
-              width={180}
-              height={50}
+              width={216}
+              height={60}
               priority
             />
           </div>
@@ -42,8 +42,8 @@ export default function SplashScreen() {
             <Image
               src="/vaskoi_logo.png"
               alt="Vaskoi"
-              width={180}
-              height={50}
+              width={216}
+              height={60}
               priority
             />
           </div>
